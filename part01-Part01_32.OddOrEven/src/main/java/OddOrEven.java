2758073908e7cd0af1a2c45d653a532eca8157f3
Eirol <76659489+eiroldelena@users.noreply.github.com>
2024-04-19 12:06:48 +0800

    Exercise 1.25 - 1.33

--- part01-Part01_32.OddOrEven/src/main/java/OddOrEven.java
+++ part01-Part01_32.OddOrEven/src/main/java/OddOrEven.java
@@ -20,5 +20,13 @@ public class OddOrEven {
         // int luku = 8
         // System.out.println( luku%2 );
         // So, by taking the modulo of a number and two you can find out if it is even or odd !
+        System.out.println("Give a number:");
+        int num = Integer.valueOf(scan.nextLine());
+        int remainder = num % 2;
+        if (remainder == 0) {
+            System.out.println("Number " + num + "is even.");
+        } else {
+            System.out.println("Number " + num + "is odd.");
+        }
     }
 }
--- part01-Part01_33.Password/src/main/java/Password.java
+++ part01-Part01_33.Password/src/main/java/Password.java
@@ -7,5 +7,13 @@ public class Password {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here 
+        String password = "Caput Draconis";
+        System.out.println("Password?");
+        String input = scan.nextLine();
+        if (input.equals (password)) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
     }
 }
