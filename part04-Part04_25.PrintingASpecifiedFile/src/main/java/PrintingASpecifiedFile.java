270a8291a3b3cc04e094069b6a4cc527e2245b2d
Eirol <76659489+eiroldelena@users.noreply.github.com>
2024-07-16 22:36:02 +0800

    Exercise 4.23 - 4.28

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -7,5 +7,19 @@ public class PrintingASpecifiedFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("What file should have its contents printed?");
+        
+        String filename = scanner.nextLine();
+        
+        try (Scanner scanFile = new Scanner(Paths.get(filename))) {
+            
+            while (scanFile.hasNextLine()) {
+                String row = scanFile.nextLine();
+                System.out.println(row);
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -1,6 +1,7 @@
 
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class NumbersFromAFile {
 
@@ -14,6 +15,24 @@ public class NumbersFromAFile {
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
 
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        try (Scanner fileReader = new Scanner(Paths.get(file))) {
+            
+            while (fileReader.hasNextLine()) {
+                int value = Integer.valueOf(fileReader.nextLine());
+                
+                if (value >= lowerBound && value <= upperBound) {
+                    list.add(value);
+                }
+            }
+            
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        System.out.println("Numbers: " + list.size());
+        
     }
 
 }
