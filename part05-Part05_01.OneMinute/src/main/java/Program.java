cc4dff8eed2550e79a2ec782b116af4d3403e4c1
Eirol <76659489+eiroldelena@users.noreply.github.com>
2024-07-21 23:04:55 +0800

    Exercise 5.1 - 5.4

---
+++ part05-Part05_01.OneMinute/src/main/java/Program.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        // You can test your program here
+        
+        Timer timer = new Timer();
+        System.out.println(timer);
+        timer.advance();
+        System.out.println(timer);
+    }
+}
